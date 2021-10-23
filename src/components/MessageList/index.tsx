adb8650933e8f0b8c6a49b5323d26b08ab78a5c4
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-10-23 20:25:19 -0300

    Changed MessageList - index.tsx to show messages received from backend

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,14 +1,25 @@
 import { api } from '../../services/api';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg';
 
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
   useEffect(() => {
-    api.get('messages/last3').then(response => {
-      console.log(response.data);
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data);
     });
   }, [])
 
@@ -17,35 +28,20 @@ export function MessageList() {
       <img src={logoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="http://github.com/deywersonp.png" alt="Deywerson Pereira" />
-            </div>
-            <span>Deywerson Pereira</span>
-          </div>
-        </li>
+        {messages.map(message => {
+          return (
+            <li className={styles.message} key={message.id}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          )
+        })}
 
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="http://github.com/deywersonp.png" alt="Deywerson Pereira" />
-            </div>
-            <span>Deywerson Pereira</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="http://github.com/deywersonp.png" alt="Deywerson Pereira" />
-            </div>
-            <span>Deywerson Pereira</span>
-          </div>
-        </li>
       </ul>
 
     </div>
