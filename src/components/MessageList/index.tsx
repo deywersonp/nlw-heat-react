a173a8dff7eccaab21f5cb6cd0f845060ea33f7e
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-10-23 20:07:03 -0300

    Created connection with backend

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,8 +1,17 @@
+import { api } from '../../services/api';
+import { useEffect } from 'react';
+
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg';
 
 export function MessageList() {
+  useEffect(() => {
+    api.get('messages/last3').then(response => {
+      console.log(response.data);
+    });
+  }, [])
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
